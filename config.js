1faaddbac7d6de1a211b69cba97adbdc960bf143
gobind0248 <gobind.bngn@gmail.com>
2025-12-17 23:25:07 +0530

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Make sure this is at the TOP of config.js
-const GITHUB_CONFIG = {
-    ENABLED: true,
-    REPO: 'gobind0248/gstutorial', // Your actual repo
-    TOKEN: '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', // Your actual token
-    BRANCH: 'main',
-    COMMIT_MESSAGE: 'Question updated by admin',
-    ADD_COMMIT_MESSAGE: 'New question added by admin'
-};
